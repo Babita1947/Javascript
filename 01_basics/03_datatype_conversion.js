1f2c89fb4f87e3f1a3f5f2a5f5363564002144ca
Babita1947 <130829271+Babita1947@users.noreply.github.com>
2023-12-09 21:34:33 +0530

    added 04_comparision.js file

--- 01_basics/03_datatype_conversion.js
+++ 01_basics/03_datatype_conversion.js
@@ -1,26 +1,26 @@
 //Example -> 1
-// let score = "33";
+let score = "33";
 // console.log(typeof score);
 // console.log(typeof(score));
-// let valueInNumber1 = Number(score);
+let valueInNumber1 = Number(score);
 // console.log(valueInNumber1);
 // console.log(typeof valueInNumber1);
 // console.log("\n");
 
 //Example -> 2
-// let num = "33abc";
+let num = "33abc";
 // console.log(typeof num);
 // console.log(typeof(num));
-// let valueInNumber2 = Number(num);
+let valueInNumber2 = Number(num);
 // console.log(valueInNumber2);  //NaN (Not A Number)
 // console.log(typeof valueInNumber2);
 // console.log("\n");
 
 // Example -> 3
-// let n = null;
+let n = null;
 // console.log(typeof n);
 // console.log(typeof(n));
-// let valueInNumber3 = Number(n);
+let valueInNumber3 = Number(n);
 // console.log(valueInNumber3);
 // console.log(typeof valueInNumber3);
 
@@ -30,8 +30,8 @@
 
 // Example -> 4
 
-// let isLoggedIn = 1;
-// let booleanIsLoggedIn = Boolean(isLoggedIn);
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
 // console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
@@ -41,11 +41,55 @@
 // Example -> 5
 let someNumber = 33;
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
 // Example -> 6
 let someString = "Babita";
 let numeric = Number(someString);
-console.log(numeric);
-console.log(typeof numeric);
+// console.log(numeric);
+// console.log(typeof numeric);
+
+// ************************ Operations *********************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3); //remainder used in cryptography
+
+let str1 = "hello";
+let str2 = " Babita";
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); // 32
+
+// console.log((3 + 4) * 5 % 3);
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;
+
+let x = 3;
+const y = x++;
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
