611cce585ab99ef430a4f612bc26c8bf8b050055
Babita1947 <130829271+Babita1947@users.noreply.github.com>
2023-12-13 17:41:03 +0530

    added some code in datatypes-summary.js file

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -41,7 +41,29 @@ let myObj = {
 const myFunction = function(){
     console.log("Hello World");
 }
-myFunction();
-console.log(typeof myFunction);
+//myFunction();
+//console.log(typeof myFunction);
 
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+//------------Memory in JavaScript------------------------
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myYoutubename = "hiteshchoudharydotcom";
+
+let anothername = myYoutubename;
+anothername = "chaiaurcode";
+
+//console.log(myYoutubename);
+//console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne;
+
+userTwo.email = "hitesh@google.com";
+console.log(userOne.email);
+console.log(userTwo.email);
